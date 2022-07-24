d31f1a7e62885ce339e0724668f48218682c077a
Nagarajan <nagarajansai2727@gmail.com>
2022-07-24 15:05:31 +0530

    formik

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -4,79 +4,127 @@ import Form from 'react-bootstrap/Form';
 import {useNavigate} from 'react-router-dom'
 import {url} from './../App'
 import axios from 'axios'
+import { useFormik } from 'formik';
+ import * as Yup from 'yup';
 
 function CreateUser() {
 
-  // window.localStorage.setItem('localData',"I am Local Storage")
-
-
-  // useEffect(()=>{
-  //   console.log("Use effect called!")
-  // })//it will be called as many times as the component re-renders
-
-  // useEffect(()=>{
-  //   console.log("Use effect called!")
-  // },[])//use effect will be called only when the component is redering for the first time
-
-  
-
-  let [firstName,setFName] = useState("")
-  let [lastName,setLName] = useState("")
-  let [email,setEmail] = useState("")
-  let [dob,setDOB] = useState("")
-  let [mobile,setMobile] = useState("")
-  let [location,setLocation] = useState("")
   let navigate = useNavigate()
-
-  
-  let handleSubmit = async()=>{
-    let data = {
-      firstName,
-      lastName,
-      email,
-      dob,
-      mobile,
-      location
-    }
+  let handleSubmit = async(data)=>{
+    console.log(data)
     let res = await axios.post(url,data)
     if(res.status===201)
       navigate('/dashboard')
     
   }
+
+  const UserForm = useFormik({
+    initialValues:{
+      firstName:"",
+      lastName:"",
+      email:"",
+      dob:"",
+      mobile:"",
+      location:""
+    },
+    validationSchema:Yup.object({
+      firstName:Yup.string().required("*Required").min(2,"First Name can not be less then 2 characters"),
+      lastName:Yup.string().required("*Required").min(2,"Last Name can not be less then 2 characters"),
+      email:Yup.string().email("Enter an Valid Email").required("*Required"),
+      dob:Yup.string(),
+      mobile:Yup.string().max(10,"Mobile number can not exceed 10 numbers").matches(/^\d{10}$/,"Enter a vaild mobile number").required("*Required"),
+      location:Yup.string()
+    }),
+    onSubmit:values=>handleSubmit(values)
+  }) 
   return<div>
-    <Form>
-    <Form.Group className="mb-3">
-        <Form.Label>First Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter First Name" onChange={(e)=>setFName(e.target.value)}/>
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Last Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter Last Name" onChange={(e)=>setLName(e.target.value)}/>
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter Email" onChange={(e)=>setEmail(e.target.value)}/>
-      </Form.Group>
+    <form onSubmit={UserForm.handleSubmit}>
+      <div className='form-group'>
+        <label>First Name</label>
+        <input
+          name='firstName'
+          type='text'
+          className='form-control'
+          placeholder='First Name'
+          onChange={UserForm.handleChange}
+          onBlur={UserForm.handleBlur}
+          value={UserForm.values.firstName}
+        />
+        {UserForm.touched.firstName && UserForm.errors.firstName?<div style={{"color":"red"}}>{UserForm.errors.firstName}</div>:<></>}
+      </div>
 
-      <Form.Group className="mb-3">
-        <Form.Label>Mobile</Form.Label>
-        <Form.Control type="text" placeholder="Enter Mobile" onChange={(e)=>setMobile(e.target.value)}/>
-      </Form.Group>
+      <div className='form-group'>
+        <label>First Name</label>
+        <input
+          name='lastName'
+          type='text'
+          className='form-control'
+          placeholder='Last Name'
+          onChange={UserForm.handleChange}
+          onBlur={UserForm.handleBlur}
+          value={UserForm.values.lastName}
+        />
+        {UserForm.touched.lastName && UserForm.errors.lastName?<div style={{"color":"red"}}>{UserForm.errors.lastName}</div>:<></>}
+      </div>
 
-      <Form.Group className="mb-3">
-        <Form.Label>DOB</Form.Label>
-        <Form.Control type="date" placeholder="dd-mm-yy" onChange={(e)=>setDOB(e.target.value)}/>
-      </Form.Group>
+      <div className='form-group'>
+        <label>Email</label>
+        <input
+          name='email'
+          type='email'
+          className='form-control'
+          placeholder='Email'
+          onChange={UserForm.handleChange}
+          onBlur={UserForm.handleBlur}
+          value={UserForm.values.email}
+        />
+        {UserForm.touched.email && UserForm.errors.email?<div style={{"color":"red"}}>{UserForm.errors.email}</div>:<></>}
+      </div>
 
-      <Form.Group className="mb-3">
-        <Form.Label>Location</Form.Label>
-        <Form.Control type="text" placeholder="Enter Location" onChange={(e)=>setLocation(e.target.value)}/>
-      </Form.Group>
+      <div className='form-group'>
+        <label>DOB</label>
+        <input
+          name='dob'
+          type='date'
+          className='form-control'
+          placeholder='Date of Birth'
+          onChange={UserForm.handleChange}
+          onBlur={UserForm.handleBlur}
+          value={UserForm.values.dob}
+        />
+        {UserForm.touched.dob && UserForm.errors.dob?<div style={{"color":"red"}}>{UserForm.errors.dob}</div>:<></>}
+      </div>
 
-      <Button variant="primary" onClick={()=>handleSubmit()}>
-        Submit
-      </Button>
-    </Form>
+      <div className='form-group'>
+        <label>Mobile</label>
+        <input
+          name='mobile'
+          type='text'
+          className='form-control'
+          placeholder='Mobile'
+          onChange={UserForm.handleChange}
+          onBlur={UserForm.handleBlur}
+          value={UserForm.values.mobile}
+        />
+        {UserForm.touched.mobile && UserForm.errors.mobile?<div style={{"color":"red"}}>{UserForm.errors.mobile}</div>:<></>}
+      </div>
+
+      <div className='form-group'>
+        <label>Location</label>
+        <input
+          name='location'
+          type='text'
+          className='form-control'
+          placeholder='Location'
+          onChange={UserForm.handleChange}
+          onBlur={UserForm.handleBlur}
+          value={UserForm.values.location}
+        />
+        {UserForm.touched.location && UserForm.errors.location?<div style={{"color":"red"}}>{UserForm.errors.location}</div>:<></>}
+      </div>
+
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
   </div>
 }
 
